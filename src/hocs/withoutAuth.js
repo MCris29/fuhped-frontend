416f5e3ecadf58e970f292a68b4a9c385abc39f7
MCris29 <crismax0629@gmail.com>
2021-08-04 12:37:17 -0500

    Redirect menu

--- src/hocs/withoutAuth.js
+++ src/hocs/withoutAuth.js
@@ -5,7 +5,7 @@ import Routes from "@/constants/routes";
  * Require the user to be unauthenticated in order to render the component.
  * If the user is authenticated, forward to the given URL.
  */
-export default function withoutAuth(WrappedComponent, location = Routes.HOME) {
+export default function withoutAuth(WrappedComponent, location = Routes.MENU) {
   // const from = history.location.state && history.location.state.from.pathname;
 
   return withAuthRedirect({
