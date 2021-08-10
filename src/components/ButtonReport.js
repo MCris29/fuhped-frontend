89f4b7030e27ac7676c1d878162a383d42a9e471
MCris29 <crismax0629@gmail.com>
2021-08-10 15:55:30 -0500

    Generate Reports

---
+++ src/components/ButtonReport.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import GenerateReport from "@/components/GenerateReport";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    backgroundColor: theme.palette.primary.second,
+    borderRadius: theme.border.default,
+    color: theme.palette.text.second,
+    textTransform: "none",
+    padding: "5px 30px",
+    margin: "0 3px",
+    "&:hover": {
+      backgroundColor: theme.palette.primary.main,
+    },
+  },
+}));
+
+const ButtonReport = (props) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Button
+        className={classes.button}
+        onClick={() =>
+          GenerateReport(
+            props.tableColumn,
+            props.tableRows,
+            props.title,
+            props.fileName
+          )
+        }
+      >
+        Generar PDF
+      </Button>
+    </>
+  );
+};
+
+export default ButtonReport;
