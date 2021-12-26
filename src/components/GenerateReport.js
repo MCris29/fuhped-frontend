a57d652b045dc5c2bd6475ecf0adaee113152ba7
MCris29 <crismax0629@gmail.com>
2021-12-26 11:22:47 -0500

    Add header in PDF report

--- src/components/GenerateReport.js
+++ src/components/GenerateReport.js
@@ -4,11 +4,34 @@ import "jspdf-autotable";
 const GenerateReport = (tableColumn, tableRows, title, fileName) => {
   const doc = new jsPDF();
 
-  doc.autoTable(tableColumn, tableRows, { startY: 20 });
+  var img = new Image();
+  img.src = "/logo_header.png";
 
-  const date = Date().split(" ");
-  const dateStr = date[0] + date[1] + date[2] + date[3] + date[4];
-  doc.text(title, 14, 15);
+  const date = new Date();
+  const dateStr =
+    date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+
+  doc.addImage(img, "png", 15, 16, 12, 15);
+
+  doc.setFontSize(14);
+  doc.text("FUNDACIÓN HALCONES POR EL DEPORTE", 55, 20);
+
+  doc.setFontSize(12);
+  doc.text("Porque el sueño, es triunfar...", 80, 28);
+
+  doc.addImage(img, "png", 185, 16, 12, 15);
+
+  doc.setDrawColor(84, 65, 96);
+  doc.setLineWidth(0.5);
+  doc.line(15, 35, 195, 35);
+
+  doc.setFontSize(11);
+  doc.text("Fecha: " + dateStr, 15, 40);
+
+  doc.setFontSize(14);
+  doc.text(title, 80, 55);
+
+  doc.autoTable(tableColumn, tableRows, { startY: 60 });
 
   doc.save(`${fileName}${dateStr}.pdf`);
 };
